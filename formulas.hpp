53da052c0336da05121bbfb58225f138ba7a838d
Elias Charrad <elias.charrad@proton.me>
2022-11-15 17:22:18 +0100

    basic code

---
+++ formulas.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int addition(int a, int b ){
+    return a + b;
+}
+int subtraction(int a, int b){
+    return a-b;
+}
+int product(int a, int b){
+    return a*b;
+}
+int division(int a, int b){
+    return a/b;
+}
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "formulas.hpp"
+
+int main(){
+    char op;
+    double a,b;
+    std::cout << "input your calculation";
+    std::cin >> a >> op >> b;
+
+    switch (op)
+    {
+    case '+':
+        std::cout << addition(a,b);
+        break;
+    case '-':
+        std::cout << subtraction(a,b);
+        break;
+    case '/':
+        std::cout << division(a,b);
+        break;
+    case '*':
+        std::cout << product(a,b);
+        break;
+    default:
+        break;
+    }
+    
+    std::cout << "contine ?:\n y / n";
+    std::string answer;
+    std::cin >> answer;
+
+    if (answer == "y")
+    {
+        main();
+    }
+    else return 0;
+    
+}
